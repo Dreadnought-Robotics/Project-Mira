5c8b45db77f9988dff4df9ed0d7068be99dee8e7
Ben <benaiahsaju@gmail.com>
2024-04-21 21:36:35 +0530

    Updated the topics for camera subscribers

--- src/mira_perception/src/Subscriber_Node1.cpp
+++ src/mira_perception/src/Subscriber_Node1.cpp
@@ -7,7 +7,7 @@ void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg) {
     try {
         cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
         // Display or process the decompressed image as needed
-        cv::imshow("Decompressed Image", cv_ptr->image);
+        cv::imshow("Front Camera View", cv_ptr->image);
         cv::waitKey(1);
     } catch (cv_bridge::Exception& e) {
         ROS_ERROR("CV_Bridge exception: %s", e.what());
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "video_subscriber1");
     ros::NodeHandle nh;
 
-    ros::Subscriber sub = nh.subscribe("/video_stream1", 1, imageCallback);
+    ros::Subscriber sub = nh.subscribe("/camera_down/image_raw/compressed", 1, imageCallback);
 
     ros::spin();
 
