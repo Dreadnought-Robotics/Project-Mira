018eab820e07939e821dd746581a652d5db25a84
Ben <benaiahsaju@gmail.com>
2024-05-07 15:08:54 +0530

    Updated Docker Launch file

--- Docker/launch.sh
+++ Docker/launch.sh
@@ -1,10 +1,8 @@
 docker run -it \
-  -v ~/Project-Mira:/root/Mira/ \
+  -v /home/ben/Mira/Project-Mira:/root/Mira/ \
   --net=host \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
-#   --cpus=4  \
-#   --memory=6g \
   --device /dev/video0:/dev/video0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   mira-docker:latest bash
